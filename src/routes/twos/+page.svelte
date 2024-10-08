2d7d8e57e4e4830d524dc6869858c8ae5ac01052
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2024-10-07 18:15:43 -0700

    twos complement calculator

---
+++ src/routes/twos/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  let bits = 32
+  let num = 0
+  $: output = num > (2 ** (bits - 1))
+    ? "number too big"
+    : Number((~-num & ((1 << bits) - 1)) + 1).toString(2).padStart(bits, "0")
+</script>
+
+<style>
+  .calc {
+    display: flex;
+    flex-direction: column;
+    width: min(600px, 80vw);
+    height: 100%;
+    place-content: center center;
+    text-align: center;
+    margin: auto;
+    font-family: sans-serif;
+    font-size: 24px;
+
+    & input {
+      font-size: 24px;
+    }
+  }
+</style>
+
+<div class="calc">
+  <div>
+    <label for="bits"># Bits: </label>
+    <input type="number" id="bits" bind:value={bits}>
+  </div>
+  <div>
+    <label for="num">Number: </label>
+    <input type="number" id="num" bind:value={num}>
+  </div>
+  <p>Output: <code>{output}</code></p>
+</div>
